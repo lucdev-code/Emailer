d8c78fb4ee9185bbe84a01835664544eed41be49
lucero <joserl.lucero.17@gmail.com>
2025-04-22 01:10:43 -0600

    move enpoint and added functions as cookies

--- app/src/backend/server.js
+++ app/src/backend/server.js
@@ -1,15 +1,70 @@
 import express from 'express'
 import { client } from './connectionPool.js'
+import Cookies from 'js-cookie'
+
 
 const app = express()
 const PORT = 3000
 
 try {
-    // endpoint admin (solo para visualizar datos)
-    app.get('/:user',
+    // sirve para manejar rutas a interfaces html
+    // app.use(express.static('src/front-end'))
+
+    // enpoint para checar que exista el email
+    app.post('/check-email/:email',
+        // ==> middleware
+        (req, res, next ) => {
+            // recuperamos el valor que se le asigno al parametro email
+            const email = req.params.email
+
+            // si el parametro ha sido envidado nullo, imprimimos el error
+            if(!email?.trim()) return res.status(500).send('No se envio correctamente')
+            
+            // implementar regex => TO-DO
+
+            // si el parametro es correcto, lo mandamos
+            next()
+        },
+        async (req, res) => {
+            const email = req.params.email
+            const queryCheckEmail = await client.query('SELECT * FROM student WHERE email = $1', [email]) 
+            if(queryCheckEmail.rows.length > 0) {
+                const user = queryCheckEmail.rows[0]
+                if(!user.email_verified) { 
+                    const verifytoggle = await client.query('UPDATE student SET email_verified = TRUE WHERE id_student = $1', [user.id_student])
+                    if(verifytoggle) {
+                        // crear variable de tiempo (current time)
+                        const date = new Date()
+                        // corregir el tiempo => TO-DO
+                        Cookies.set('emailUsertoSetPassword', email, {expires: date.getMinutes() + 3})
+                        console.log(`Tienes de ${date.getMinutes()} a ${date.getMinutes() + 3} para establecer tu contraseña`)
+                    }
+                 } else {
+                    // enviar a login directamente
+                 }
+            }
+        }
+    )
+
+    // app.post('/set-password', 
+    //     // middleware
+    //     (req, res, next) => {
+    //         const { email, password } = req.body
+
+    //         if(!email?.trim() || !password?.trim()) return res.status(500).send('Envio incorrecto')
+            
+    //         next()
+    //     },
+    //     (req, res) => {
+
+    //     }
+    // )
+
+     // endpoint admin (solo para visualizar datos)
+     app.get('/admin/:user',
         // => middleware
         async (req, res, next) => {
-            // obtenemos el parametro de user
+            // recuperamos el valor que se le asigno al parametro user
             const userName = req.params.user
 
             // ejecutamos una query donde checamos si existe el usuario
@@ -30,11 +85,14 @@ try {
 
             // si obtenemos un error al obtener los datos, imprimimos el error
             if(!result) return res.send('Hubo un error al obtener los datos')
+                
+            // si los datos son obtenidos correctamente, imprimimos los datos
             res.send(result.rows)
+
         }
     )
 
-    // endpoint to add student
+    // endpoint para añadir estudiantes (solo admins)
     app.post('/students/add/:nameS/:lastS/:grade',
         // => middleware
         (req, res, next) => {
@@ -54,55 +112,6 @@ try {
                 const correo = nameS.toLocaleLowerCase().replace(/\s+/g, '') + '.' + lastS.toLocaleLowerCase().replace(/\s+/g, '') + '@school.dev';
                 res.send(`Te has registrado exitosamente, tu correo es: ${correo}`);
             }else return res.status(500).send('Lo siento no se ha completado el registro, intenta mas tarde!')
-        }
-    )
-
-    app.post('/check-email/:email',
-        // ==> middleware
-        (req, res, next ) => {
-            const email = req.params.email
-            if(!email?.trim()) return res.status(500).send('No se envio correctamente')
-            next()
-        },
-        async (req, res) => {
-            const email = req.params.email
-            const queryCheckEmail = await client.query('SELECT * FROM student WHERE email = $1', [email]) 
-            if(queryCheckEmail.rows.length > 0) {
-                const user = queryCheckEmail.rows[0]
-                if(!user.email_verified) { 
-                    const verifytoggle = await client.query('UPDATE student SET email_verified = TRUE WHERE id_student = $1', [user.id_student])
-                    if(verifytoggle) {
-                        res.send('Se ha confirmado el correo')
-                        // return res.redirect(`/check-email/${user.email}/password`)
-                    }
-                 } else {
-                    res.send('Ya se ha confirmado el correo anteriormente')
-                 }
-            }
-        }
-    )
-
-    app.post('/check-email/:email/password', 
-        // middleware
-        (req, res, next) => {
-            const { email, password } = req.body
-
-            if(!email?.trim() || !password?.trim()) return res.status(500).send('Envio incorrecto')
-            
-            next()
-        },
-        (req, res) => {
-
-        }
-    )
-
-    // enpoint para logearse
-    app.post('/login/:email/:password', 
-        async (req, res, next) => {
-            const query = client.query
-        },
-        async (req, res) => {
-
         }
     )
 
