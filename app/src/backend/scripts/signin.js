2384ca9c3a12b9aec2d30a000aa5e3d2cc701f5c
lucero <joserl.lucero.17@gmail.com>
2025-04-24 19:19:38 -0600

    added a request to verify the cookie to access to signin

--- app/src/backend/scripts/signin.js
+++ app/src/backend/scripts/signin.js
@@ -1,3 +1,25 @@
+async function signin(e) {
+    e.preventDefault()
+    const password = document.getElementById('passwordSignin').value
+
+    if (!password?.trim()) return console.log('Los datos no han sido llenados correctamente')
+
+        const response = await fetch('http://localhost:3000/login', {
+            method: 'POST',
+            credentials: 'include',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ password })
+        });
+    
+        if (!response.ok) return console.log('Error en la petición');
+    
+        const result = await response.json();
+        
+        if(result.status !== 'OK') return console.log(result.message)
+        else return window.location.href = '../../front-end/html/index.html'
+}
+
+
 
 document.addEventListener('DOMContentLoaded', async() => {
 
@@ -12,5 +34,17 @@ document.addEventListener('DOMContentLoaded', async() => {
 
     const infoCookie = await searchCookie.json()
 
-    console.log(infoCookie)
+    if (infoCookie.status !== 'OK') {
+        alert('No tienes permiso para estar aqui')
+        window.location.href = '../../front-end/html/index.html'
+    }
+
+    const formLogin = document.getElementById('login')
+
+    if(!formLogin) return console.log('No existe el form')
+
+    const inputEmail = document.getElementById('emailSignin')
+    inputEmail.value = infoCookie.email
+
+    formLogin.addEventListener('submit', signin)
 })
