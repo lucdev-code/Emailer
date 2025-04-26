49b3a3562f2a638a1e1e9138295206d0f0fbdadf
lucero <joserl.lucero.17@gmail.com>
2025-04-25 22:19:12 -0600

    added logic for verify email (need to check again) and added function set password

--- app/src/back-end/controllers/auth.controller.js
+++ app/src/back-end/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import { pool } from '../bd/connectionPool.js'; 
 import dotenv from 'dotenv'
+
 dotenv.config()
 
 // validamos el email despues del middleware
@@ -13,12 +14,38 @@ export const verifyEmailStudent = async (req, res) => {
 
         if(checkEmail.rows.length > 0) {
             
-            if(checkEmail.rows[0].email_verified === true) return res.json({checkEmail, message: 'Se encontro el email!', status: 'OK', email_verified: true})
-            else return res.json({checkEmail, message: 'Se encontro el email!', status: 'OK', email_verified: false})
+            if(checkEmail.rows[0].email_verified === true && checkEmail.rows[0].password !== null) {
+                res.cookie('user_email', email, {
+                    maxAge: 3 * 60 * 1000
+                });
+                return res.json({message: 'Se encontro el email!', status: 'OK', email_verified: true})
+            }
+            else {
+                const setemailverified = pool.query('UPDATE student SET email_verified = true')
+                res.cookie('user_email', email, {
+                    maxAge: 3 * 60 * 1000
+                });
+                return res.json({message: 'Se encontro el email!', status: 'OK', email_verified: false})
+            }
         }
         else return res.json({message: 'No se encontro el correo', status: 'NOT FIND'})
 
     } catch(error) {
         res.json({mesage: 'Hubo algun error, favor de intentar mas tarde', status: 'ERROR CATCH'})
     }
+}
+
+export const setPassword = async (req, res) => {
+    const emailCookie = req.cookies.user_email
+    const password = req.body.password
+
+    if(!emailCookie || !password?.trim()) return res.json({message: 'No se encontro la cookie o no se enviaron los datos correctamente', status: 'FAIL_2'})
+    else return res.json({message: 'Todo en orden', emailCookie, password})
+} 
+
+export const getCookie = (req, res) => {
+    const emailCookie = req.cookies.user_email
+
+    if(!emailCookie) return res.json({message: 'No se encontro la cookie', status: 'NOT FIND'})
+    else return res.json({mail: emailCookie, message: 'Si se encontro la cookie', status: 'OK'})
 }
