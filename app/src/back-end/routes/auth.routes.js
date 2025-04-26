0ce26e01875a3584f1eeda8da524bc80451f511f
lucero <joserl.lucero.17@gmail.com>
2025-04-25 22:20:01 -0600

    import and added routes for auth

--- app/src/back-end/routes/auth.routes.js
+++ app/src/back-end/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import { verifyEmailStudent_middleware } from '../middlewares/auth_middlewares.js'
-import { verifyEmailStudent } from '../controllers/auth.controller.js'
+import { verifyEmailStudent_middleware, setPassword_middleware, getCookie_middleware } from '../middlewares/auth_middlewares.js'
+import { verifyEmailStudent, setPassword, getCookie } from '../controllers/auth.controller.js'
 
 const router = express.Router()
 
 // agregamos el metodo que realizara la ruta, seguida de su middleware, y por ultimo el enpoint real
 router.post('/student/validateEmail', verifyEmailStudent_middleware, verifyEmailStudent)
+router.get('/student/cookie', getCookie_middleware, getCookie)
+router.post('/student/set-pasword', setPassword_middleware, setPassword)
 
 
 
