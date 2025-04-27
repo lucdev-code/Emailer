6d6e81eb20e960325c8d21d57db5b9754514cf9b
lucero <joserl.lucero.17@gmail.com>
2025-04-26 22:34:03 -0600

    not special

--- app/src/back-end/middlewares/admin_middlewares.js
+++ app/src/back-end/middlewares/admin_middlewares.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 export const checkAdmin_middleware = async (req, res, next) => {
     const admin = req.params.admin
-
+z
     const queryAdminCheck = await pool.query('SELECT * FROM admins WHERE name_a = $1', [admin])
 
     if(queryAdminCheck.rows.length === 0) return res.json({message: 'No tienes permisos para ver esto', status: 'NOT ALLOWED'}) 
