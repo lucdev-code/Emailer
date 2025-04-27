a567e96f142ebf5178d65e582b4b5297ef733bcd
lucero <joserl.lucero.17@gmail.com>
2025-04-26 20:59:58 -0600

    added a new controller for a route

--- app/src/back-end/controllers/auth.controller.js
+++ app/src/back-end/controllers/auth.controller.js
@@ -3,6 +3,13 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+export const getCookie = (req, res) => {
+    const emailCookie = req.cookies.user_email
+
+    if(!emailCookie) return res.json({message: 'No se encontro la cookie', status: 'NOT FIND'})
+    else return res.json({mail: emailCookie, message: 'Si se encontro la cookie', status: 'OK'})
+}
+
 // validamos el email despues del middleware
 export const verifyEmailStudent = async (req, res) => {
     try {
@@ -10,18 +17,17 @@ export const verifyEmailStudent = async (req, res) => {
 
         if(!email?.trim()) return res.json({message: 'No se envio el correo', status: 'FAIL'})
 
-        const checkEmail = await pool.query('SELECT * FROM student WHERE email = $1', [email])
+        const checkEmailQuery = await pool.query('SELECT * FROM student WHERE email = $1', [email])
 
-        if(checkEmail.rows.length > 0) {
+        if(checkEmailQuery.rows.length > 0) {
             
-            if(checkEmail.rows[0].email_verified === true && checkEmail.rows[0].password !== null) {
+            if(checkEmailQuery.rows[0].email_verified === true && checkEmailQuery.rows[0].password !== null) {
                 res.cookie('user_email', email, {
                     maxAge: 3 * 60 * 1000
                 });
                 return res.json({message: 'Se encontro el email!', status: 'OK', email_verified: true})
             }
             else {
-                const setemailverified = pool.query('UPDATE student SET email_verified = true')
                 res.cookie('user_email', email, {
                     maxAge: 3 * 60 * 1000
                 });
@@ -40,12 +46,18 @@ export const setPassword = async (req, res) => {
     const password = req.body.password
 
     if(!emailCookie || !password?.trim()) return res.json({message: 'No se encontro la cookie o no se enviaron los datos correctamente', status: 'FAIL_2'})
-    else return res.json({message: 'Todo en orden', emailCookie, password})
+    
+    const setPassQuery = await pool.query('UPDATE student SET password = $1, email_verified = $2 WHERE email = $3', [password, true, emailCookie])
+    if(setPassQuery) return res.json({message: 'Todo en orden', status: 'OK'})
 } 
 
-export const getCookie = (req, res) => {
+export const login = async (req, res) => {
     const emailCookie = req.cookies.user_email
+    const password = req.body.password
 
-    if(!emailCookie) return res.json({message: 'No se encontro la cookie', status: 'NOT FIND'})
-    else return res.json({mail: emailCookie, message: 'Si se encontro la cookie', status: 'OK'})
-}
+    if(!emailCookie || !password?.trim()) return res.json({message: 'No se encontro la cookie o no se enviaron los datos correctamente', status: 'FAIL_2'})
+    
+    const loginQuery = await pool.query('SELECT * FROM student WHERE email = $1 AND password = $2', [emailCookie, password])
+    if(loginQuery.rows.length > 0) return res.json({message: 'Si se encontro el usuario', status: 'OK'})
+    else return res.json({message: 'No se encontro el usuario', status: 'FAIL'})
+}   
--- app/src/back-end/middlewares/auth_middlewares.js
+++ app/src/back-end/middlewares/auth_middlewares.js
@@ -2,6 +2,17 @@ import { pool } from '../bd/connectionPool.js';
 import dotenv from 'dotenv'
 dotenv.config()
 
+
+// verificamos que exista la cookie
+export const getCookie_middleware = (req, res, next) => {
+    const email = req.cookies.user_email
+
+    if(!email) return res.json({message: 'No se ha encontrado la cookie', status: 'FAIL'})
+
+    next()
+}
+
+
 // verificamos que el email no sea nulo y le pasamos por un test sobre el regex para ver si tiene la estructura solicitada
 export const verifyEmailStudent_middleware = async (req, res, next) => {
     const email = req.body.email
@@ -15,19 +26,21 @@ export const verifyEmailStudent_middleware = async (req, res, next) => {
     next()
 }
 
+// verificamos que los campos no sean nullos
 export const setPassword_middleware = (req, res, next) => {
+    const emailCookie = req.cookies.user_email
     const password = req.body.password
 
-    if(!password?.trim()) return res.json({message: 'No se ha enviado correctamente la informacion', status: 'FAIL'})
+    if(!emailCookie || !password?.trim()) return res.json({message: 'No se ha enviado correctamente la informacion', status: 'FAIL'})
 
     next()
 }
 
-
-export const getCookie_middleware = (req, res, next) => {
+export const login_middleware = (req, res, next) => {
     const email = req.cookies.user_email
+    const password = req.body.password
 
-    if(!email) return res.json({message: 'No se ha encontrado la cookie', status: 'FAIL'})
-
+    if(!email || !password?.trim()) return res.json({message: 'No se ha enviado correctamente la informacion', status: 'FAIL'})
+    
     next()
 }
