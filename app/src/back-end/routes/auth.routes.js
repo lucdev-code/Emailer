21277b28e68e85690151698c0d954bf743af37c1
lucero <joserl.lucero.17@gmail.com>
2025-04-26 21:04:39 -0600

    fix route of script on front end and added a new route

--- app/src/back-end/routes/auth.routes.js
+++ app/src/back-end/routes/auth.routes.js
@@ -1,13 +1,24 @@
 import express from 'express'
-import { verifyEmailStudent_middleware, setPassword_middleware, getCookie_middleware } from '../middlewares/auth_middlewares.js'
-import { verifyEmailStudent, setPassword, getCookie } from '../controllers/auth.controller.js'
+import { 
+    verifyEmailStudent_middleware, 
+    setPassword_middleware, 
+    getCookie_middleware,
+    login_middleware
+} from '../middlewares/auth_middlewares.js'
+
+import { 
+    verifyEmailStudent, 
+    setPassword, 
+    getCookie, 
+    login } from '../controllers/auth.controller.js'
 
 const router = express.Router()
 
 // agregamos el metodo que realizara la ruta, seguida de su middleware, y por ultimo el enpoint real
 router.post('/student/validateEmail', verifyEmailStudent_middleware, verifyEmailStudent)
 router.get('/student/cookie', getCookie_middleware, getCookie)
-router.post('/student/set-pasword', setPassword_middleware, setPassword)
+router.patch('/student/set-pasword', setPassword_middleware, setPassword)
+router.post('/student/login', login_middleware, login)
 
 
 
