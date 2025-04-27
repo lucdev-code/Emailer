a41676d48d05657752091bd9c4bfefbc841fad33
lucero <joserl.lucero.17@gmail.com>
2025-04-26 22:33:24 -0600

    added trycatch to managa errors

--- app/src/back-end/controllers/auth.controller.js
+++ app/src/back-end/controllers/auth.controller.js
@@ -1,13 +1,17 @@
-import { pool } from '../bd/connectionPool.js'; 
+import { pool } from '../bd/connectionPool.js';
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 export const getCookie = (req, res) => {
-    const emailCookie = req.cookies.user_email
+    try {
+        const emailCookie = req.cookies.user_email
 
-    if(!emailCookie) return res.json({message: 'No se encontro la cookie', status: 'NOT FIND'})
-    else return res.json({mail: emailCookie, message: 'Si se encontro la cookie', status: 'OK'})
+        if (!emailCookie) return res.json({ message: 'No se encontro la cookie', status: 'NOT FIND' })
+        else return res.json({ mail: emailCookie, message: 'Si se encontro la cookie', status: 'OK' })
+    } catch (error) {
+        return res.json({message: 'Error al intentar', status: 'ERROR CATCH'})
+    }
 }
 
 // validamos el email despues del middleware
@@ -15,49 +19,58 @@ export const verifyEmailStudent = async (req, res) => {
     try {
         const email = req.body.email
 
-        if(!email?.trim()) return res.json({message: 'No se envio el correo', status: 'FAIL'})
+        if (!email?.trim()) return res.json({ message: 'No se envio el correo', status: 'FAIL' })
 
         const checkEmailQuery = await pool.query('SELECT * FROM student WHERE email = $1', [email])
 
-        if(checkEmailQuery.rows.length > 0) {
-            
-            if(checkEmailQuery.rows[0].email_verified === true && checkEmailQuery.rows[0].password !== null) {
+        if (checkEmailQuery.rows.length > 0) {
+
+            if (checkEmailQuery.rows[0].email_verified === true && checkEmailQuery.rows[0].password !== null) {
                 res.cookie('user_email', email, {
                     maxAge: 3 * 60 * 1000
                 });
-                return res.json({message: 'Se encontro el email!', status: 'OK', email_verified: true})
+                return res.json({ message: 'Se encontro el email!', status: 'OK', email_verified: true })
             }
             else {
                 res.cookie('user_email', email, {
                     maxAge: 3 * 60 * 1000
                 });
-                return res.json({message: 'Se encontro el email!', status: 'OK', email_verified: false})
+                return res.json({ message: 'Se encontro el email!', status: 'OK', email_verified: false })
             }
         }
-        else return res.json({message: 'No se encontro el correo', status: 'NOT FIND'})
+        else return res.json({ message: 'No se encontro el correo', status: 'NOT FIND' })
 
-    } catch(error) {
-        res.json({mesage: 'Hubo algun error, favor de intentar mas tarde', status: 'ERROR CATCH'})
+    } catch (error) {
+        res.json({ mesage: 'Hubo algun error, favor de intentar mas tarde', status: 'ERROR CATCH' })
     }
 }
 
 export const setPassword = async (req, res) => {
-    const emailCookie = req.cookies.user_email
-    const password = req.body.password
+   try {
+        const emailCookie = req.cookies.user_email
+        const password = req.body.password
 
-    if(!emailCookie || !password?.trim()) return res.json({message: 'No se encontro la cookie o no se enviaron los datos correctamente', status: 'FAIL_2'})
-    
-    const setPassQuery = await pool.query('UPDATE student SET password = $1, email_verified = $2 WHERE email = $3', [password, true, emailCookie])
-    if(setPassQuery) return res.json({message: 'Todo en orden', status: 'OK'})
-} 
+        if (!emailCookie || !password?.trim()) return res.json({ message: 'No se encontro la cookie o no se enviaron los datos correctamente', status: 'FAIL_2' })
+
+        const setPassQuery = await pool.query('UPDATE student SET password = $1, email_verified = $2 WHERE email = $3', [password, true, emailCookie])
+        if (setPassQuery) return res.json({ message: 'Todo en orden', status: 'OK' })
+
+   } catch(error) {
+        return res.json({message: 'Error al intentar', status: 'ERROR CATCH'})
+   }
+}
 
 export const login = async (req, res) => {
+  try { 
     const emailCookie = req.cookies.user_email
     const password = req.body.password
 
-    if(!emailCookie || !password?.trim()) return res.json({message: 'No se encontro la cookie o no se enviaron los datos correctamente', status: 'FAIL_2'})
-    
+    if (!emailCookie || !password?.trim()) return res.json({ message: 'No se encontro la cookie o no se enviaron los datos correctamente', status: 'FAIL_2' })
+
     const loginQuery = await pool.query('SELECT * FROM student WHERE email = $1 AND password = $2', [emailCookie, password])
-    if(loginQuery.rows.length > 0) return res.json({message: 'Si se encontro el usuario', status: 'OK'})
-    else return res.json({message: 'No se encontro el usuario', status: 'FAIL'})
+    if (loginQuery.rows.length > 0) return res.json({ message: 'Si se encontro el usuario', status: 'OK' })
+    else return res.json({ message: 'No se encontro el usuario', status: 'FAIL' })
+  } catch(error) {
+    return res.json({message: 'Error al intentar', status: 'ERROR CATCH'})
+  }
 }   
