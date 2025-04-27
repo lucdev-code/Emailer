64360653e58d6306e2a67e083c80adacf90d8496
lucero <joserl.lucero.17@gmail.com>
2025-04-26 22:57:01 -0600

    nothing else

--- app/src/back-end/routes/admin.routes.js
+++ app/src/back-end/routes/admin.routes.js
@@ -5,7 +5,8 @@ import { getStudents } from '../controllers/admin.controller.js'
 const router = express.Router()
 
 // agregamos el metodo que realizara la ruta, seguida de su middleware, y por ultimo el enpoint real
-router.get('/getStudents/:admin', checkAdmin_middleware, getStudents)
+
+router.get('/getStudents', checkAdmin_middleware, getStudents)
 
 
 export default router
