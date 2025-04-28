2bdf88b478aaf7a2e6bc6a2468c3d4cc686a3308
lucero <joserl.lucero.17@gmail.com>
2025-04-27 22:47:12 -0600

    log route added to routes

--- app/src/back-end/routes/admin.routes.js
+++ app/src/back-end/routes/admin.routes.js
@@ -1,12 +1,13 @@
 import express from 'express'
-import { checkAdmin_middleware } from '../middlewares/admin_middlewares.js'
-import { getStudents } from '../controllers/admin.controller.js'
+import { checkAdmin_middleware, login_admin_middleware } from '../middlewares/admin_middlewares.js'
+import { getStudents, login_admin } from '../controllers/admin.controller.js'
 
 const router = express.Router()
 
 // agregamos el metodo que realizara la ruta, seguida de su middleware, y por ultimo el enpoint real
 
 router.get('/getStudents', checkAdmin_middleware, getStudents)
+router.post('/log', login_admin_middleware, login_admin)
 
 
 export default router
