fec29841b250f8614a143a4f371e045ec264037c
lucero <joserl.lucero.17@gmail.com>
2025-04-27 22:47:28 -0600

    fecth signin admin enpoint

--- app/src/back-end/scripts/signin_admin.js
+++ app/src/back-end/scripts/signin_admin.js
@@ -6,6 +6,18 @@ async function fetchToAdminLogin(e) {
     if(!email?.trim() || !password?.trim()) return alert('Todos los campos son obligatorios')
 
     // hacer el fetch
+    const fetchtoadminlogin = await fetch('http://localhost:3000/admin/log', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email: email, password: password })
+    })
+
+    if(!fetchtoadminlogin.ok) return alert('Error en el fetch')
+
+    const resultFecthAdminLog = await fetchtoadminlogin.json()
+
+    if(resultFecthAdminLog.status !== 'OK') return window.location.href = '../../front-end/html/unauthorized.html'
+    else return window.location.href = '../../front-end/html/admin_page.html'
 }
 
 document.addEventListener('DOMContentLoaded', () => {
