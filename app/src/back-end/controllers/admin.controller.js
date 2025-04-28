eb3c2681ac5a4d58e248d80aaf57dc8d1ff1f266
lucero <joserl.lucero.17@gmail.com>
2025-04-27 22:46:54 -0600

    login controlller added

--- app/src/back-end/controllers/admin.controller.js
+++ app/src/back-end/controllers/admin.controller.js
@@ -3,7 +3,20 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const login_admin = async (req, res) => {
-  // logica
+  try {
+    const email_admin = req.body.email
+    const password_admin = req.body.password
+
+    // query
+    const login_admin_query = await pool.query('SELECT * FROM admins WHERE email_a = $1 AND password_a = $2', [email_admin, password_admin])
+
+    // verificamos
+    if(login_admin_query.rows.length > 0) return res.json({message: 'Si se encontro el correo del admin', status: 'OK'})
+    else return res.json({message: 'No se encontro el usuario', status: 'NOT FIND'})
+
+  }catch(error) {
+    return res.json({message: 'Hubo un error al intentar', status: 'ERROR CATCH'})
+  }
 }
 
 export const getStudents = async (req, res) => {
